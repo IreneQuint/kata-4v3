8fa808c570c42316819404a2a0223374e0e4e5af
usuario <usuario@10.22.143.13>
2015-10-27 09:50:46 +0000

    kata 4v3 primera versión

---
+++ src/kata/pkg4v3/Kata4v3.java
@@ -0,0 +1,31 @@
+package kata.pkg4v3;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Kata4v3 {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String from = "C:\\Users\\usuario\\Desktop\\kata 4.txt";
+        String to = "C:\\Users\\usuario\\Desktop\\kata 4 (1).txt";
+        
+        InputStream input = new BufferedInputStream(new FileInputStream(new File(from)));
+        OutputStream output = new BufferedOutputStream(new FileOutputStream(new File(to)));
+        
+        while(true){
+            int read = input.read();
+            if(read < 0) break;
+            output.write(read);
+        }
+        input.close();
+        output.flush();
+        output.close();
+    }
+}
